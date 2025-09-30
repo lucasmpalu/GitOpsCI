b91254740444fcd489d700df4a2f40fad04b6ab7
Lucas Palú <111322631+lucasmpalu@users.noreply.github.com>
2025-09-30 10:35:57 -0300

    test CI/CD

--- backend/index.php
+++ backend/index.php
@@ -1,41 +1,21 @@
-<?php<?php
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 
-header("Access-Control-Allow-Origin: *");header("Access-Control-Allow-Origin: *");
+include 'conexao.php';
 
-header("Access-Control-Allow-Headers: *");header("Access-Control-Allow-Headers: *");
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$fecha_nacimiento = $_POST["fecha_nacimiento"];
+$telefono = $_POST["telefono"];
+$email = $_POST["email"];
 
+$stmt = $link->prepare("INSERT INTO pacientes (nombre, apellido, fecha_nacimiento, telefono, email) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sssss", $nombre, $apellido, $fecha_nacimiento, $telefono, $email);
 
-
-include 'conexao.php';include 'conexao.php';
-
-
-
-$nombre = $_POST["nombre"];$nombre = $_POST["nombre"];
-
-$apellido = $_POST["apellido"];$apellido = $_POST["apellido"];
-
-$fecha_nacimiento = $_POST["fecha_nacimiento"];$fecha_nacimiento = $_POST["fecha_nacimiento"];
-
-$telefono = $_POST["telefono"];$telefono = $_POST["telefono"];
-
-$email = $_POST["email"];$email = $_POST["email"];
-
-
-
-$stmt = $link->prepare("INSERT INTO pacientes (nombre, apellido, fecha_nacimiento, telefono, email) VALUES (?, ?, ?, ?, ?)");$stmt = $link->prepare("INSERT INTO pacientes (nombre, apellido, fecha_nacimiento, telefono, email) VALUES (?, ?, ?, ?, ?)");
-
-$stmt->bind_param("sssss", $nombre, $apellido, $fecha_nacimiento, $telefono, $email);$stmt->bind_param("sssss", $nombre, $apellido, $fecha_nacimiento, $telefono, $email);
-
-
-
-if ($stmt->execute()) {if ($stmt->execute()) {
-
-    echo "Paciente registrado com sucesso";    echo "Paciente registrado com sucesso";
-
-} else {} else {
-
-    echo "Error: " . $stmt->error;    echo "Error: " . $stmt->error;
-
-}}
-
-?>?>
+if ($stmt->execute()) {
+    echo "Paciente registrado com sucesso";
+} else {
+    echo "Error: " . $stmt->error;
+}
+?>
